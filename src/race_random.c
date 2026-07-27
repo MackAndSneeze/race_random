40c7e629fd1e3eefbcec75d7831143d1f4a20280
Will Taylor <wat1@sfu.ca>
2026-07-27 15:15:52 -0700

    I can't believe I'm actually fixing bugs for this

--- src/race_random.c
+++ src/race_random.c
@@ -4,12 +4,15 @@
 #include <string.h>
 #include <unistd.h>
 
-int cnt = 0;
+#ifndef VARIANCE_OFFSET
+#define VARIANCE_OFFSET 100000
+#endif
+int cnt;
 
 void *count_thread(void *arg) {
   int *p_deviation = (int *)arg;
   int deviation = *p_deviation;
-  for (int i = 0; i < deviation; i++) {
+  for (int i = 0; i < deviation * VARIANCE_OFFSET; i++) {
     cnt++;
   }
   return NULL;
@@ -19,8 +22,9 @@ int race_random(int mini, int maxi, int variance_coefficient) {
   if (mini >= maxi) {
     return 0;
   }
+  cnt = 0;
 
-  int deviation = (maxi * variance_coefficient);
+  int deviation = ((maxi - mini) * variance_coefficient);
 
   pthread_t thread1, thread2;
 
@@ -30,7 +34,7 @@ int race_random(int mini, int maxi, int variance_coefficient) {
   pthread_join(thread1, NULL);
   pthread_join(thread2, NULL);
 
-  int rand_i = mini + ((cnt - deviation) % (maxi - mini));
+  int rand_i = mini + ((cnt - deviation) % (maxi - mini + 1));
 
   return rand_i;
 }
--- src/tests.c
+++ src/tests.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 int main() {
-  int result1 = race_random(1, 10, 1000000);
-  int result2 = race_random(1, 10, 10);
-  int result3 = race_random(100, 300, 1000000);
+  int result1 = race_random(1, 10, 1);
+  int result2 = race_random(1, 10, 100);
+  int result3 = race_random(100, 300, 10);
+  int result4 = race_random(67, 69, 420);
 
-  printf("%d\n%d\n%d\n", result1, result2, result3);
+  printf("%d\n%d\n%d\n%d\n", result1, result2, result3, result4);
 }
